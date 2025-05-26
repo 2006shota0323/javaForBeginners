afd751b15579dc42269a5c3876dcdad570a1624b
Shota0323 <shotawatanabe03230@gamil.com>
2025-05-26 11:20:02 +0900

    RectAndOvalの追加、くるまの絵

---
+++ src/RectAndOval.java
@@ -0,0 +1,13 @@
+
+public class RectAndOval  extends MyFrame{
+	public void run()
+	{
+		fillRect(30,100,200,40);
+		
+		fillRect(50,60,160,60);
+		
+		fillOval(50,120,50,50);
+		
+		fillOval(150,120,50,50);
+	}
+}
