cd2dc45db512c3e181583d106984da22ec8d05e3
Shota0323 <shotawatanabe03230@gamil.com>
2025-06-02 10:02:25 +0900

    5-3,5-ex-5-ex2の追加

--- src/Animation.java
+++ src/Animation.java
@@ -51,33 +51,105 @@ public class Animation extends MyFrame{
 //			 sleep(0.1);
 //		 }
 		
+//		5-3
+//		for(int i=1; i<=3; i++)
+//		{
+//			while(x<=200)
+//			{
+//				clear();
+//				setColor(0,128,0);
+//				fillOval(x,y,100,100);
+//				x+=5;
+//				sleep(0.1);
+//			}
+//			while(x>=30)
+//			{
+//				clear();
+//				setColor(0,128,0);
+//				fillOval(x,y,100,100);
+//				x-=5;
+//				sleep(0.1);
+//			}
+//			
+//			
+//		}
+		
+//		5-EX
+//		while(true)
+//		{
+//			while(x<=200)
+//			{
+//				clear();
+//				setColor(0,128,0);
+//				fillOval(x,y,100,100);
+//				x+=5;
+//				sleep(0.1);
+//			}
+//			while(y<=250)
+//			{
+//				clear();
+//				setColor(0,128,0);
+//				fillOval(x,y,100,100);
+//				y+=5;
+//				sleep(0.1);
+//			}
+//			while(x>=30)
+//			{
+//				clear();
+//				setColor(0,128,0);
+//				fillOval(x,y,100,100);
+//				x-=5;
+//				sleep(0.1);
+//			}
+//			while(y>=100)
+//			{
+//				clear();
+//				setColor(0,128,0);
+//				fillOval(x,y,100,100);
+//				y-=5;
+//				sleep(0.1);
+//			}	
 		
 		
-		for(int i=1; i<=3; i++)
-		{
-			while(x<=200)
-			{
-				clear();
-				setColor(0,128,0);
-				fillOval(x,y,100,100);
-				x+=5;
-				sleep(0.1);
-			}
-			while(x>=30)
-			{
-				clear();
-				setColor(0,128,0);
-				fillOval(x,y,100,100);
-				x-=5;
-				sleep(0.1);
-			}
-			
-			
-		}
-		
-		
-		
-		
+//	       5-EX2
+//		    while(true)
+//		    {
+//		    	while(x<=100)
+//					{
+//						clear();
+//						setColor(0,128,0);
+//						fillOval(x,y,100,100);
+//						x+=5;
+//						sleep(0.1);
+//					}
+//					while(y<=125)
+//					{
+//						clear();
+//						setColor(0,128,0);
+//						fillOval(x,y,100,100);
+//						y+=5;
+//						sleep(0.1);
+//					}
+//					while(x>=30)
+//					{
+//						clear();
+//						setColor(0,128,0);
+//						fillOval(x,y,100,100);
+//						x-=5;
+//						sleep(0.1);
+//					}
+//		    	    while(y<=250)
+//		    	    {
+//		    	    	clear();
+//						setColor(0,128,0);
+//						fillOval(x,y,100,100);
+//						y+=5;
+//						sleep(0.1);
+//		    	    }
+//		    	
+//		    	 x=30;
+//		    	 y=100;
+//		    }
 		
 		
 		
