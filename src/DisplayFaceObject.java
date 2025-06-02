1114bdc7301d3e68444e81a3c40104252c5efa1f
Shota0323 <shotawatanabe03230@gamil.com>
2025-06-02 11:45:20 +0900

    コンデンサの追加、6-2

--- src/DisplayFaceObject.java
+++ src/DisplayFaceObject.java
@@ -2,15 +2,14 @@
 public class DisplayFaceObject extends MyFrame{
 	public void run()
 	{
-		Face facel=new Face();
-		facel.x=100;
-		facel.y=50;
+		Face facel=new Face(50,50);
 		facel.draw(this);
 		
-		Face facel2=new Face();
-		facel2.x=200;
-		facel2.y=150;
+		Face facel2=new Face(200,50);
 		facel2.draw(this);
+		
+		Face facel3=new Face(125,150);
+		facel3.draw(this);
 	}
 
 }
